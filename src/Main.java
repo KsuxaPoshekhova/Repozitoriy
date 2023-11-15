58206409fe540fed057dffff1d8a5f3040e2fb88
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-15 22:00:40 +0300

    Komit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-int price = 10_000;
-int miles = 20;
-System.out.println(price / miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); 
+        System.out.println(miles);
     }
 }
